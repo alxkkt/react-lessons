3ee399be3ce439a38c1957ea6449d65a01a704c2
Oleksii Kikot <oleksii.kikot@yahoo.com>
2022-05-19 15:16:11 +0200

    pushed everthing to git

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Accordion from "./components/Accordion/Accordion.jsx";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <Accordion />
+      </div>
+    </div>
+  );
+}
+
+export default App;
