d353d5b1962fe7d1b589843ac0c7d3b0385ccc1f
Oleksii Kikot <oleksii.kikot@yahoo.com>
2022-05-26 14:32:23 +0200

    added `add more` button

--- src/components/AbilitiesList/AbilitiesList.jsx
+++ src/components/AbilitiesList/AbilitiesList.jsx
@@ -1,7 +1,9 @@
 import { useState, useEffect } from "react";
-import styles from "./AbilitiesList.module.css";
 
 import { getAbiities } from "../../shared/service/pokemons";
+import Button from "../Button";
+
+import styles from "./AbilitiesList.module.css";
 
 const LIMIT = 10;
 
@@ -9,31 +11,38 @@ const AbilitiesList = ({}) => {
   const [items, setItems] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
+  const [page, setPage] = useState(1);
 
   useEffect(() => {
     setLoading(true);
-    fetchItems();
-  }, []);
 
-  const fetchItems = async () => {
-    try {
-      const data = await getAbiities({ limit: LIMIT });
-      setItems(data.results);
-      setLoading(false);
-    } catch (error) {
-      setError(error.message);
-      setLoading(false);
-    }
+    const fetchItems = async () => {
+      try {
+        const data = await getAbiities({ limit: LIMIT * page });
+        setItems(data.results);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchItems();
+  }, [page]);
+
+  const loadMore = () => {
+    setPage((prevPage) => prevPage + 1);
   };
 
-  const elements = items.map(({ name }) => <li>Name: {name}</li>);
+  const elements = items.map(({ name }) => <li key={name}>Name: {name}</li>);
 
   return (
     <div>
       <h1>Abilities</h1>
-      {loading && <p>...Loading...</p>}
       {error && <p>...{error}...</p>}
       <ul>{elements}</ul>
+      <Button onClick={loadMore} text={"Load More"} />
+      {loading && <p>...Loading...</p>}
     </div>
   );
 };
